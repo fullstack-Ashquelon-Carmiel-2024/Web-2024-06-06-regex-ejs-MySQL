9a465a27f74b8d01fa6c788311bf25e4785febae
NatalieShif <natalia.shif@gmail.com>
2024-06-06 11:08:56 +0300

    getSampleHomePage in modele/home.js

--- nodejs/yummy/model/home.js
+++ nodejs/yummy/model/home.js
@@ -2,8 +2,33 @@ module.exports = {
   
   getSampleHomePage: (req,res) => {
 
-    res.render('small-index.ejs',
-      {title: 'Yummy'})
+    res.render('index.ejs',
+      {title: 'Yummy',
+       foods: [ 
+        {
+         id: 1,
+         name: 'chumus masbacha',
+         cosher_type: 'parve',
+         cuisine_id: 1,
+         image: 'chummus-masbacha.jpg', 
+         kalories: 550, 
+         prepare_time_min: 120, 
+         ingredients: 'chumus, tchina, oil',  
+         type_of_dish: 'main'
+        },
+        {
+         id: 2,
+         name: 'sushi',
+         cosher_type: 'parve',
+         cuisine_id: 2,
+         image: 'sushi.jpeg',
+         kalories: 250, 
+         prepare_time_min: 90, 
+         ingredients: 'rice, nuri, avocado',  
+         type_of_dish: 'main'
+        }
+       ]
+      })
 
   },
 
